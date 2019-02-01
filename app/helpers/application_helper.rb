0648fe448067a2bfe4249379615418b50f59fa99
Adam Pope <adam1.pope@gmail.com>
2019-02-01 16:14:12 -0600

    Updated styles and added map for contact page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-  def login_helper
+  def login_helper style 
     if current_user.is_a?(GuestUser)
-      (link_to 'Register', new_user_registration_path) +
-      "<br>".html_safe +
-      (link_to 'Login', new_user_session_path)  
+      (link_to 'Register', new_user_registration_path, class: style) +
+      " ".html_safe +
+      (link_to 'Login', new_user_session_path, class: style)  
     else 
-      link_to 'Logout', destroy_user_session_path, method: :delete 
+      link_to 'Logout', destroy_user_session_path, method: :delete, class: style 
     end  
   end 
 
@@ -16,7 +16,7 @@ module ApplicationHelper
     end
   end
 
-  # This works with the custom gem I created 
+  # This works with the custom gem I created. takes two arguments. 
   def copyright_generator
     PopeViewTool::Renderer.copyright "Adam Pope", "All rights reserved"
   end 
