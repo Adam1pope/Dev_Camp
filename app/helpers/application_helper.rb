08b2182a759818ac46ab8524ad176b55b25d2866
Adam Pope <adam1.pope@gmail.com>
2019-02-07 18:29:23 -0600

    Refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -60,4 +60,21 @@ module ApplicationHelper
     "active" if current_page? path #current_page? special rails method
   end  
 
+
+  def alerts 
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert 
+    end 
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title:  "Adam Pope Portfolio", sticky: false)
+  end 
+
+
+
+
+
 end
